cb7af373547e9bb2685a36479649912e4386a51e
EZMED247 <ezmed247@gmail.com>
2024-06-05 12:48:14 +0300

    Improvment to the categorical search to use 'i'

--- controllers/search.js
+++ controllers/search.js
@@ -2,10 +2,23 @@ const Medicine = require('../models/Med')
 const { StatusCodes } = require('http-status-codes')
 const NotFoundError = require('../errors')
 
+const searchMedicinesByIndication = async (indication, res) => {
+    try {
+        const medicines = await Medicine.find({
+            indications: new RegExp(indication, 'i')
+        });
+        if (medicines.length === 0) {
+            throw new NotFoundError('Medicine not found');
+        }
+        res.status(StatusCodes.OK).json(medicines);
+    } catch (err) {
+        res.status(StatusCodes.NOT_FOUND).json({ message: 'Medicine not found' });
+    }
+}
 
 const medSearch = async (req, res) => {
     try {
-        const { query } = req.body
+        const { query } = req.body;
         const medicines = await Medicine.find({
             $or: [
                 { medName: new RegExp(query, 'i') },
@@ -13,66 +26,19 @@ const medSearch = async (req, res) => {
                 { medNameArabic: new RegExp(query, 'i') },
                 { activeIngredientsArabic: new RegExp(query, 'i') }
             ]
-        })
-        res.status(StatusCodes.OK).json(medicines)
+        });
+        res.status(StatusCodes.OK).json(medicines);
     } catch (err) {
-        throw new NotFoundError('Medicine not found')
+        throw new NotFoundError('Medicine not found');
     }
 }
 
-const medHeadache = async (req, res) => {
-    try {
-        const medicines = await Medicine.find({ "indications": "Headache" })
-        res.status(StatusCodes.OK).json(medicines)
-    } catch (err) {
-        res.status(StatusCodes.NOT_FOUND).json({ message: 'Medicine not found' })
-    }
-}
-
-const medVitamins = async (req, res) => {
-    try {
-        const medicines = await Medicine.find({ "indications": "Vitamins" })
-        res.status(StatusCodes.OK).json(medicines)
-    } catch (err) {
-        res.status(StatusCodes.NOT_FOUND).json({ message: 'Medicine not found' })
-    }
-}
-
-const medHairloss = async (req, res) => {
-    try {
-        const medicines = await Medicine.find({ "indications": "Hair Loss" })
-        res.status(StatusCodes.OK).json(medicines)
-    } catch (err) {
-        res.status(StatusCodes.NOT_FOUND).json({ message: 'Medicine not found' })
-    }
-}
-
-const medBeauty = async (req, res) => {
-    try {
-        const medicines = await Medicine.find({ "indications": "Beauty" })
-        res.status(StatusCodes.OK).json(medicines)
-    } catch (err) {
-        res.status(StatusCodes.NOT_FOUND).json({ message: 'Medicine not found' })
-    }
-}
-
-const medDiet = async (req, res) => {
-    try {
-        const medicines = await Medicine.find({ "indications": "Diet" })
-        res.status(StatusCodes.OK).json(medicines)
-    } catch (err) {
-        res.status(StatusCodes.NOT_FOUND).json({ message: 'Medicine not found' })
-    }
-}
-
-const medCold = async (req, res) => {
-    try {
-        const medicines = await Medicine.find({ "indications": "Cold" })
-        res.status(StatusCodes.OK).json(medicines)
-    } catch (err) {
-        res.status(StatusCodes.NOT_FOUND).json({ message: 'Medicine not found' })
-    }
-}
+const medHeadache = (req, res) => searchMedicinesByIndication('Headache', res);
+const medVitamins = (req, res) => searchMedicinesByIndication('Vitamins', res);
+const medHairloss = (req, res) => searchMedicinesByIndication('Hair Loss', res);
+const medBeauty = (req, res) => searchMedicinesByIndication('Beauty', res);
+const medDiet = (req, res) => searchMedicinesByIndication('Diet', res);
+const medCold = (req, res) => searchMedicinesByIndication('Cold', res);
 
 module.exports = {
     medSearch,
